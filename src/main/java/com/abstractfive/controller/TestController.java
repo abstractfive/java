f94f892860f6ea4d1f2c4c9fe5fe06233089c8b9
lumingliang <lumingliang@qh-speed.com>
2018-04-11 18:07:26 +0800

    1：添加Thymeleaf模板 demo 2：修改配置文件.yml

--- src/main/java/com/abstractfive/controller/TestController.java
+++ src/main/java/com/abstractfive/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.abstractfive.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
