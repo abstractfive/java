603219acd03507ab6df36ef53f8f0c246c9d54f0
zhangzhen <zhangzhen@bjjy.com>
2018-09-28 15:58:21 +0800

    第一次测试

--- src/main/java/com/abstractfive/JavaApplication.java
+++ src/main/java/com/abstractfive/JavaApplication.java
@@ -2,11 +2,16 @@ package com.abstractfive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello nmsl 李子宁 ";
 	}
 }
